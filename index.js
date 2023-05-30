b98961c55449e8912c9160e130855323b03b6599
Abdulrahman <albakkar997@gmail.com>
2023-05-30 03:12:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str){
+    return str.replace(/[^a-zA-Z0-9-' ]/g, "")
+  }
+  static titleize(str){
+    const lowerCaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    return str.toLowerCase().split(' ').map((word, index) => {
+      if(index !== 0 && lowerCaseWords.includes(word)) {
+        return word;
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  }
 }
